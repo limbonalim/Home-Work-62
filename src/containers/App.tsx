ea8e811974bb9167b5e73dbe8a2e02195f7f61e0
Zhuravlev Petr <zeterfrost@gmail.com>
2023-11-24 13:24:19 +0600

    added content in Home container

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,3 +1,4 @@
+import Home from './Home/Home.tsx';
 
 
 const App = () => {
@@ -6,20 +7,20 @@ const App = () => {
     <>
       <nav className="navbar navbar-expand-lg bg-danger">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">InnovateHub</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
-              <a className="nav-link" href="#">Features</a>
-              <a className="nav-link" href="#">Pricing</a>
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link" href="#">About us</a>
+              <a className="nav-link" href="#">Contacts</a>
             </div>
           </div>
         </div>
       </nav>
+      <Home/>
     </>
   );
 };
